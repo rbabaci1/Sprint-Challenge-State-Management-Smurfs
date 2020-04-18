6ebcf95eaab2ca37843460ef7b70fb7169d5c219
rabah babaci <4rabah@gmail.com>
2020-04-18 11:01:23 -0700

    style the form

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Smurfs from './Smurfs';
+import Smurfs from './components/Smurfs';
+import SmurfForm from './components/SmurfForm';
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1>SMURFS! 2.0 W/ Redux</h1>
+
+        <SmurfForm />
 
         <Smurfs />
       </div>
--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function SmurfForm() {
-  return <div className='smurf-form'></div>;
+  return (
+    <div className='smurf-form'>
+      <form>
+        <label>
+          What's your name:
+          <input id='name' type='text' placeholder='...name' required />
+        </label>
+
+        <label>
+          What's your age:
+          <input id='age' type='number' placeholder='...age' required />
+        </label>
+
+        <label>
+          What's your height:
+          <input id='height' type='number' placeholder='...height' required />
+        </label>
+      </form>
+    </div>
+  );
 }
