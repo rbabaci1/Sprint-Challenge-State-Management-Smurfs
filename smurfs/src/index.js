4a4591b0381f67df5ae03256b830c72c6466dabc
rabah babaci <4rabah@gmail.com>
2020-04-18 09:49:08 -0700

    setup the Redux store and provide it to the App

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
+import { reducer } from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
