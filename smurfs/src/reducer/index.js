5cafeb7347d82f226f69e3a7ab409bcc44041382
rabah babaci <4rabah@gmail.com>
2020-04-18 10:08:33 -0700

    in the reducer, add a case to the ERROR action

--- smurfs/src/reducer/index.js
+++ smurfs/src/reducer/index.js
@@ -1,4 +1,4 @@
-import { LOADING, SUCCESS } from '../actions';
+import { LOADING, SUCCESS, ERROR } from '../actions';
 
 const initialState = {
   smurfs: [],
@@ -12,12 +12,19 @@ export const reducer = (state = initialState, { type, payload }) => {
       return {
         ...state,
         loading: true,
+        error: '',
       };
     case SUCCESS:
       return {
         ...state,
         smurfs: payload,
         loading: false,
+      };
+    case ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: 'Request failed, please try again!',
       };
     default:
       return state;
