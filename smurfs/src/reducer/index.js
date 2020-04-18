b03fc5bd3faaf1c8f33d734e863215bc3893f45b
rabah babaci <4rabah@gmail.com>
2020-04-18 10:06:59 -0700

    in the reducer, add a case to the SUCCESS action

--- smurfs/src/reducer/index.js
+++ smurfs/src/reducer/index.js
@@ -1,4 +1,4 @@
-import { LOADING } from '../actions';
+import { LOADING, SUCCESS } from '../actions';
 
 const initialState = {
   smurfs: [],
@@ -12,6 +12,12 @@ export const reducer = (state = initialState, { type, payload }) => {
       return {
         ...state,
         loading: true,
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        smurfs: payload,
+        loading: false,
       };
     default:
       return state;
