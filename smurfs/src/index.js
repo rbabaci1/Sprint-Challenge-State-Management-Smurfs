d158077ef5e19e69845743f7cc0ca24f6bdeca1f
rabah babaci <4rabah@gmail.com>
2020-04-18 09:43:46 -0700

    import all the needed tools to create the redux store in index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
