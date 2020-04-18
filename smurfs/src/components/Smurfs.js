9bb450e0505cdd3154932c5d509856595c274fe8
rabah babaci <4rabah@gmail.com>
2020-04-18 09:41:44 -0700

    test render Smurfs in App

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Smurfs() {
-  return <div className='smurfs'></div>;
+  return (
+    <div className='smurfs'>
+      <h1>Smurfs</h1>
+    </div>
+  );
 }
